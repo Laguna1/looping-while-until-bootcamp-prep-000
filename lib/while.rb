b48920be370733d06b8641404cec54823c3d060a
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-12 22:40:39 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 def using_while
   levitation_force = 6
 	while levitation_force < 10
-	puts "Wingardium Liviosa"
-	levitation_force += 1
-	
+	  puts "Wingardium Liviosa"
+	  levitation_force += 1
+	end
   #your code here
 end
 
