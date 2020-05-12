e64ef70d1b8b6a783ba414b3b7b8140f4f45c19b
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-12 22:41:40 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,15 +4,5 @@ def using_while
 	  puts "Wingardium Leviosa"
 	  levitation_force += 1
 	end
-  #your code here
 end
 
-# count = 1
-
-# while count <= 5 #do
-#     puts count
-#     count += 1
-#     sleep 0.5
-# end
-# sleep 0.5
-# puts "It was a counter to 5."
